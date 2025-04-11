d288f010487defaf08a1b7cc910ae3a3aa4b6d54
Vitoria Fortes <fortesvitoria@gmail.com>
2025-04-11 19:33:06 -0300

    atualiza exercicio

--- 05abrEx04a.py
+++ 05abrEx04a.py
@@ -1,11 +1,12 @@
 print('Faça os programas individuais para calcular os perímetros e as áreas das figuras ')
 print('geométricas abaixo. Alguns programas precisarão de vários valores de entrada.')
 print('FIGURA A')
-lado1 = float(input("Digite o primeiro lado do seu triangulo: "))
-lado2 = float(input("Digite o segundo lado do seu triangulo: "))
-lado3 = float(input("Digite o terceiro lado do seu triangulo: "))
-peri = lado1 + lado2 + lado3
+ladoA = float(input("Digite o primeiro lado do seu triangulo: "))
+ladoB = float(input("Digite o segundo lado do seu triangulo: "))
+ladoC = float(input("Digite o terceiro lado do seu triangulo: "))
+peri = ladoA + ladoB + ladoC
 altura = float(input("Digite a altura do seu triangulo: "))
-area = (lado1 * altura)/2
+area = (ladoB * altura)/2
 
-print(f"O seu perimetro é de {peri:.2f}cm e sua area pe de {area:.2f}cm^2")
+print (f"O lado A é {ladoA}, o lado B é {ladoB}, o lado C é {ladoC} e o a altura é {altura}")
+print(f"O seu perimetro é de {peri:.2f}cm e sua area é de {area:.2f}cm^2")
