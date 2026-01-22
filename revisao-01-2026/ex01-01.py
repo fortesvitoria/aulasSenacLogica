9db19cc899d83642a5c2d897b263952d9b505eec
Vitoria Fortes <fortesvitoria@gmail.com>
2026-01-22 14:26:30 -0300

    revisando

---
+++ revisao-01-2026/ex01-01.py
@@ -0,0 +1,61 @@
+print('SEQÜÊNCIAS SIMPLES / CONDICIONAL')
+print('1. Ler dois números e imprimir as variáveis na mesma ordem que foram digitadas:')
+
+
+
+print('----------------------------------------------')
+print('2. Escreva um algoritmo que leia dois números que deverão ser colocados, respectivamente nas variáveis vA e vB. ')
+
+
+
+print('----------------------------------------------')
+print('3. Ler dois valores numéricos e escrever a soma destes.')
+
+
+
+print('----------------------------------------------')
+print('4. Ler três valores numéricos e escrever a média aritmética.')
+
+
+
+print('----------------------------------------------')
+print('5. Ler um conjunto de 5 dados numéricos e imprimir sua soma e sua média.')
+
+
+
+print('----------------------------------------------')
+print('6. Faça um algoritmo que leia valores para as variáveis a, b e c e mostre o resultado da seguinte expressão: ( a – b ) * c')
+
+
+
+print('----------------------------------------------')
+print('7. Faça um algoritmo que mostre o resultado da expressão abaixo:')
+print('(( x – 5) * y) – z')
+print('Obs: Ler valores para as variáveis x, y e z.')
+
+
+
+print('----------------------------------------------')
+print('8. Fazer um algoritmo para ler duas notas, os pesos de cada nota e mostrar a média ponderada.')
+print('                               (nota 1 * peso da nota 1) + (nota 2 * peso da nota 2)')
+print('Cálculo da Média Ponderada = ------------------------------------------------------------------------')
+print('                                                    soma dos pesos')
+
+
+
+print('----------------------------------------------')
+print('9. Escrever um algoritmo para ler uma temperatura em Fahrenheit e apresentá-la convertida em graus Centígrados.')
+print('                           (Fahrenheit – 32) x 5')
+print('Fórmula: Centígrados = ----------------------------')
+print('                                    9')
+
+
+
+print('----------------------------------------------')
+print('10. Maria quer saber quantos litros de gasolina precisa colocar em seu carro e quanto vai gastar para fazer uma viagem até a casa de sua irmã.')
+print('Dados extras:')
+print('- Distância da casa de Maria até sua irmã : 520 km - Seu carro consome 12 Km/litro de combustível.')
+print('- Ela abastece sempre no mesmo posto, onde o preço da gasolina é R$ 4,50 o litro.')
+print('Desenvolva um algoritmo onde o usuário digite a distância, o consumo e o valor do litro de combustível,')
+print('com estes dados o algoritmo deverá calcular a quantidade de litros de combustível para a viagem e o custo da viagem.')
+print('--------------------------------------')
---
+++ revisao-01-2026/ex01.py
@@ -0,0 +1,112 @@
+# print(' 1. Ler dois números e imprimir as variáveis na mesma ordem que foram digitadas.')
+
+# numbers = []
+
+# for i in range(2):
+#     number = int(input("Digite um número: "))
+#     numbers.append(number)
+# print(numbers)
+
+# print(f"\n\n OU\n\n")
+
+# for n in numbers:
+#     print(n) 
+
+
+# print(f"\n\n ----------------------------- \n\n")
+
+# print('1.a. Ler dois números e imprimir as variáveis na ordem inversa que foram digitadas.')
+
+# numeros = []
+
+# for x in range(2):
+#     numero = int(input("Digite um número: "))
+#     numeros.append(numero)
+
+# for m in reversed(numeros):
+#     print(m)
+
+
+# print('2. Ler dois valores numéricos e escrever a soma destes.')
+# numeros = []
+
+# for i in range(2):
+#     numero = int(input("Digite um número: "))
+#     numeros.append(numero)
+
+# soma = sum(numeros)
+# print(soma)
+
+
+# print('3. Ler dois valores numéricos e escrever a  subtração destes.')
+# numeros = []
+# for i in range(2):
+#     numero = int(input("Digite um número: "))
+#     numeros.append(numero)
+
+# resultado = numeros[0] - numeros[1]
+# print(resultado)
+
+
+# print("4.Ler dois valores numéricos e escrever a  multiplicação destes.")
+# numeros = []
+
+# for i in range(2):
+#     numero = int(input("Digite um número: "))
+#     numeros.append(numero)
+
+# resultado = numeros[0]*numeros[1]
+# print(resultado)
+
+
+
+# print ('5.Ler dois valores numéricos e escrever a  divisão destes.')
+# numeros = []
+
+# for i in range(2):
+#     numero = int(input("Digite um valor: "))
+#     numeros.append(numero)
+
+# resultado = numeros[0]/numeros[1]
+# print(resultado)
+
+
+# print('6. Ler três valores numéricos e escrever a média aritmética.')
+# numeros = []
+
+# for i in range(3):
+#     numero = int(input("Digite um valor: "))
+#     numeros.append(numero)
+
+# qtd = len(numeros)
+# soma = sum(numeros)
+
+# resultado = soma/qtd
+
+# print(resultado)
+
+
+
+# print('7. Ler um conjunto de 5 dados numéricos e imprimir sua soma e sua média.')
+# numeros = []
+
+# for i in range(5):
+#     numero = int(input("Digite um valor: "))
+#     numeros.append(numero)
+
+# qtd = len(numeros)
+# soma = sum(numeros)
+# media = soma/qtd
+
+# print(f"Média: {media}\n")
+# print(f"Soma: {soma}")
+
+
+
+
+print(f'8. Escreva um algoritmo que leia dois números que deverão ser colocados, respectivamente nas variáveis a e b.')
+
+a = int(input("Digite o numero: "))
+b = int(input("Digite um número: "))
+
+print(f"a = {a}; b = {b}")
